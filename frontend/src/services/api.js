af4165f6bb332bfa047355b56f3603a3fb6ef4a7
AjiRahmanNugraha <ajirahman215@gmail.com>
2025-08-08 22:18:22 +0700

    pp

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://10.0.2.125:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
-});
+})
 
-export default api;
+export default api

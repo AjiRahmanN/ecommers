36a635429e24de4083fcb94e50732128dfb00144
AjiRahmanNugraha <ajirahman215@gmail.com>
2025-08-09 09:35:21 +0700

    port

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 // Configure CORS to allow requests from fronten
 app.use(
   cors({
-    origin: ["http://13.211.55.73", "http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://13.211.55.73", "http://localhost:3000", "http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
